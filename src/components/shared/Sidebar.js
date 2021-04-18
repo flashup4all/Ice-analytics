1f04558a8f2acd0a8412c6806418bc6f11926502
Bardeson Lucky <flashup4all@gmail.com>
2021-04-18 08:11:08 +0100

    Develop (#10)
    * added feature screen
    
    * Update README.md
    
    * created prospective and active pages (#1)
    
    Co-authored-by: Benjamin Guma <benjaminguma.bg@gmail.com>
    
    * added proper rouiting functions
    
    * created prospective and active pages (#2)
    
    Co-authored-by: Benjamin Guma <benjaminguma.bg@gmail.com>
    
    * updates
    
    * updates
    
    * updates
    
    * Feature dashboard (#9)
    
    * created prospective and active pages
    
    * added config for vercel deployment
    
    * made ui responsive plus handled browser issues
    
    * another ui change
    
    * another ui change
    
    Co-authored-by: Benjamin Guma <benjaminguma.bg@gmail.com>
    
    Co-authored-by: Benjamin Guma <benjaminguma.bg@gmail.com>
    Co-authored-by: Benjamin Guma <46363151+benjaminguma@users.noreply.github.com>

--- src/components/shared/Sidebar.js
+++ src/components/shared/Sidebar.js
@@ -38,7 +38,7 @@ const sideBarData = [
   },
 ];
 
-const breakpoint = 900;
+const breakpoint = 950;
 
 const Sidebar = () => {
   const width = useResize ();
@@ -73,7 +73,7 @@ const Sidebar = () => {
             >
               {/* this span will be used to close the sidebar */}
               <span className="hamburger open" onClick={close} />
-              <ul className="sidebar_list mb-2">
+              <ul className="sidebar_list  mt-2">
                 {sideBarData.map ((data, index) => (
                   <SidebarLink
                     key={index}
